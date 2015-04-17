1358c4c728a02961ff2e6ae1936dab6a245e6b7a
Joao Pedro Dias <jpdias@live.com.pt>
2015-04-17 18:03:39 +0100

    working read

--- Node/script.js
+++ Node/script.js
@@ -1,14 +1,19 @@
-var fs = require("fs"),
-  readline = require("readline"),
-  csv = require("fast-csv");
+var csv = require('fast-csv'); // require fast-csv module
+var fs = require('fs'); // require the fs, filesystem module
+var uniqueindex = 0; // just an index for our array
+var dataJSON = {}; // our JSON object, (make it an array if you wish)
 
 var CaidePorto = "content/Caide-Porto.csv";
 var PortoCaide = "content/Porto-Caide.csv";
 
+
+
 csv
-  .fromPath(PortoCaide)
+  .fromPath(PortoCaide, {
+    delimiter: ';'
+  })
   .on("data", function(data) {
-    console.log(data);
+    console.log(data[0]);
   })
   .on("end", function() {
     console.log("done");
